c30a2fa3f3e9e6fa3f05fe479afed4482a5ed1f7
smitzkar <karl.e.schmitz@hotmail.com>
2022-11-27 20:50:31 +0100

    Clean up formatting

--- Nolindrom.java
+++ Nolindrom.java
@@ -1,4 +1,5 @@
 public class Nolindrom {
+    
     public static long inverse(long input) {
 
         String inputstring = String.valueOf(input);
@@ -14,9 +15,11 @@ public class Nolindrom {
             outputarray[i] = aux * (long) Math.pow(10, (l-1 - i));
 
         }
+
         for(int i = 0; i < outputarray.length; i++){
             output = output + outputarray[i];
         }
+
         return output;
     }
 
@@ -56,34 +59,37 @@ public class Nolindrom {
 
         for(int i = 1; i <= upperlimit; i++){
 
-        zahl = (long) i;
-        long erg = zahl;
-        long r = 0;
-        long count = 0;
+            zahl = (long) i;
+            long erg = zahl;
+            long r = 0;
+            long count = 0;
 
         
-        do{
-            if (Long.MAX_VALUE - erg < inverse(erg)){
-                ifnotterminate[i] = true;
-                break;
-            } else { 
-
-            zahl = erg;
-            r = inverse(erg);
-            erg = erg + r;
-            count++;
+            do {
+                if (Long.MAX_VALUE - erg < inverse(erg)){
+                    ifnotterminate[i] = true;
+                    break;
+                } 
+                else { 
+                    zahl = erg;
+                    r = inverse(erg);
+                    erg = erg + r;
+                    count++;
+                }
+            }
+            while(checkifpalindrom(erg) == false);
+            // System.out.println("Zahl: " + i + " Palindrom: " + erg + " Iterationen: " + count);
+        
         }
-    }while(checkifpalindrom(erg) == false);
-    //System.out.println("Zahl: " + i + " Palindrom: " + erg + " Iterationen: " + count);
-}
-int numberofnumbers = 0;
-//System.out.println("these numbers: ");
-for(int i = 1; i <= upperlimit; i++){
-    if(ifnotterminate[i] == true){
-        System.out.println(i);
-        numberofnumbers++;
+
+        int numberofnumbers = 0;
+        // System.out.println("these numbers: ");
+        for(int i = 1; i <= upperlimit; i++){
+            if(ifnotterminate[i] == true){
+                System.out.println(i);
+                numberofnumbers++;
+            }
+        }
+        // System.out.println("there are: " + numberofnumbers + " such numbers");
     }
-}
-//System.out.println("there are: " + numberofnumbers + " such numbers");
-}
 } 
