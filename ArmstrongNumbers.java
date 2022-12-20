e0fc7c795635ce73fcc50ccc0a43d9515f964e50
Jędrek <jedrek@MacBook-Pro-7.local>
2022-12-20 04:22:24 +0100

    ArmstrongNumbers done

---
+++ ArmstrongNumbers.java
@@ -0,0 +1,55 @@
+public class ArmstrongNumbers {
+	public static void main(String[] args) {
+		int[] test = giveArmstrongNumbers(30);
+		// 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407, 1634
+		printArray(test);
+	}
+
+	public static boolean isArmstrongNumber(int number) {
+		String numberString = Integer.toString(number);
+		int [] numberArray = new int[numberString.length()];
+		int sum = 0;
+		int temp = number;
+		boolean output = false;
+
+		for(int i = 0; i < numberArray.length; i++){
+			numberArray[i] = temp % 10;
+			temp = temp/10;
+		}
+		
+		for(int i = 0; i < numberArray.length; i++){
+			sum = (int) sum + (int) Math.pow(numberArray[i], numberArray.length);
+		}
+		
+		if(sum == number){
+			output = true;
+		}
+
+		return output;
+	}
+
+	public static int[] giveArmstrongNumbers(int n) {
+		int[] output = new int[n];
+		int i = 0;
+		int count = 0;
+	
+		do{
+			if(isArmstrongNumber(i)){
+				output[count] = i;
+				count++;
+				i++;
+			} else {
+				i++;
+			}
+		}while(count < n);
+		
+		return output;
+	}
+
+	private static void printArray(int[] a) {
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i]);
+			System.out.print(i < a.length - 1 ? ", " : "\n");
+		}
+	}
+}
